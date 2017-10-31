9843634961fdb905246e773be895cbe4e5fa60cf
Anuroop Thomas <anuroopbthomas@gmail.com>
2017-10-30 20:37:41 -0700

    logfinal was created which is the better and improved version :100:
    This should be the final update I think

--- README.md
+++ README.md
@@ -1,21 +1,70 @@
 # Log Analysis Project
-## This file contains some very important information, if not owner please
-## ignore the following text below
 
+This project basically takes a given database and does certain sql Queries
+in order to successfully arrange the data for addressed ways to have it
+arranged
 
+## Directions
+
+## Requirements
+- python3
+- Vagrant
+
+## Virtual Machine And DATABASE
+1. Install Vagrant
+2. Clone the Fullstack-Nanodegree-VM
+3. Download the newsdata.sql file from Udacity
+4. Place this file within the Fullstack-Nanodegree-VM
+5. Open terminal/command prompt and redirect to the Fullstack-Nanodegree-VM
+6. Run 'vagrant up' (whenever your computer restarts you have to run this)
+7. Run 'vagrant ssh' to open the VM
+8. Redirect to where the newsdata.sql file is in the VM
+9. Run 'psql -d news -f newsdata.sql'
+10. To connect to this database run 'psql -d news'
+
+## News Views
+### Due to the README file, whenever 'all' is put in these sql queries, it means the asterisk
+1. Connect to the news database
+2. Create the first view by running
+'create view autharticle_view as select title, author,count(all)
+        as views from articles,log where
+        log.path like concat('%',articles.slug) group by articles.title,
+        articles.author order by views desc;'
+3. Create the second view by running
+'create view error_view as select date(time),
+        round(100.0*sum(case log.status when '200 OK'
+        then 0 else 1 end)/count(log.status),2) as "Percent Error"
+        from log group by date(time)
+        order by "Percent Error" desc;'
+
+## RUNNING THE Program
+To execute the code follow these Directions
+1. Download this code and save it in the VM directory (Note: You only need to download logfinal.py)
+2. Once you are open within the VM and redirected into the same directory as the python program run 'python3 logfinal.py'
+3. You will get a text result with the correct results! :)
+
+## NOTE INFORMATION BELOW IS IRRELEVANT BUT
+## IF YOU WISH TO UNDERSTAND THE DATABASE
+## YOU MAY READ THIS
 ## Tables in 'news' Database
 
 ### articles
 author - int (3)
+- Foreign key for authors'
 title - text (Bad things gone, say good people)
 slug - text (bad-things-gone)
 lead - text (All bad things have gone away, according to a poll of good people, Thursday.)
 body - text (Bad things are a thing of the bad......)
 time - timestamp with time zone (2016-08-15 18:55:10.814316+00)
 id - integer (23)
+- 23 - 30 are all articles
 
 ### authors
 name - text (Ursula La Multa)
+- Ursula La Multa (1)
+- Rudolf von Treppenwitz (2)
+- Anonymous Contributor (3)
+- Markoff Chaney (4)
 bio - text (Ursula La Multa is an expert...)
 id - integer (1)
 
@@ -23,6 +72,8 @@ id - integer (1)
 path - text (/)
 ip - inet (198.51.100.195)
 method - text (GET)
+- 'GET' is the only value for method
 status - text (200 OK)
+- only values for this are ('200 OK' and '404 NOT FOUND')
 time - timestamp with time zone (2016-07-01 7:00:00+00)
 id - int (1678923)
---
+++ logfinal.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+# FINAL SOLUTION FOR THE LOG ANALYSIS Udacity Project
+
+# Import of Pyscopg2 module
+# Declaration of DB_NAME which
+# is the news database provided
+import psycopg2
+DB_NAME = "news"
+
+# The view created to be able to get the title, and author and views
+view1 = """create view autharticle_view as select title, author,count(*)
+        as views from articles,log where
+        log.path like concat('%',articles.slug) group by articles.title,
+        articles.author order by views desc;"""
+
+# The view created to be able to get the date and the percent error
+view2 = """create view error_view as select date(time),
+        round(100.0*sum(case log.status when '200 OK'
+        then 0 else 1 end)/count(log.status),2) as "Percent Error"
+        from log group by date(time)
+        order by "Percent Error" desc;"""
+
+# Query for the three most popular articles by article name and views
+query1 = "select title, views from article_view limit 3;"
+
+# Query for the three most popular authors by name and views
+query2 = """select authors.name, sum(autharticle_view.views) as views from
+         autharticle_view,authors where authors.id = autharticle_view.author
+         group by authors.name order by views desc;"""
+
+# Query for everytime on a single day when there are a greater than
+# 1% that there is an error that day
+query3 = "select * from error_view where \"Percent Error\" > 1;"
+
+# Declaring Each query to be a dict variable type so that I can
+# efficiently store the titles and results of the queries
+
+# Query1
+query1_result = dict()
+query1_result['title'] = "\nThe 3 most popular articles of all time:\n"
+# Query2
+query2_result = dict()
+query2_result['title'] = """\nThe most popular article authors of
+all time:\n"""
+# Query3
+query3_result = dict()
+query3_result['title'] = """\nDays with more than 1% of request that
+lead to an error:\n"""
+
+
+# function to get the query results with the input of query
+def get_query_results(query):
+    """This function takes an input of query which is
+    a string variable with the sql command to run
+    and return the results of the command"""
+    db = psycopg2.connect(database=DB_NAME)
+    log = db.cursor()
+    log.execute(view1)
+    log.execute(view2)
+    log.execute(query)
+    results = log.fetchall()
+    db.close()
+    return results
+
+
+def print_query_results(query_result):
+    """Since the formatting of the first two queries are
+    the same format, this is how both of those queries will
+    be displayed and outputted
+
+    query_result will be the results from the function get_query_results
+    """
+    print (query_result['title'])
+    for result in query_result['results']:
+        print (str(result[0]) + ' --- ' + str(result[1]) + ' views')
+
+
+def print_error_results(query_result):
+    """Since the formatting of the first two queries are
+    the same format, this is how the error query will be
+    displayed and outputted
+
+    query_result will be the results from the function get_query_results
+    """
+    print (query_result['title'])
+    for result in query_result['results']:
+        print (str(result[0]) + ' --- ' + str(result[1]) + ' %')
+
+
+# Calls the function get_query_results and stores it within
+# results in each respective variable
+query1_result['results'] = get_query_results(query1)
+query2_result['results'] = get_query_results(query2)
+query3_result['results'] = get_query_results(query3)
+
+# Calls the functions which will print the titles and results
+print_query_results(query1_result)
+print_query_results(query2_result)
+print_error_results(query3_result)
